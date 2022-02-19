9109c27fa498cc557b7eadf5b86f257f1cdc60d3
Gevorg Harutyunyan <39996731+gevorgharutyunyan@users.noreply.github.com>
2022-02-19 23:39:12 +0400

    Added some comments to simplify or clean out the code

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import datetime as dt
 
 
+# For each function class need a docstring to explain usage of that.
+# For the Record class I will write it as an example and please add docstring for left classes.
+# In PEP standards need two empty lines before class declaration.
+
+
 class Record:
     def __init__(self, amount, comment, date=''):
+
         self.amount = amount
         self.date = (
             dt.datetime.now().date() if
@@ -13,6 +19,7 @@ class Record:
 
 class Calculator:
     def __init__(self, limit):
+        # Add docstrings for this class.
         self.limit = limit
         self.records = []
 
@@ -21,7 +28,13 @@ class Calculator:
 
     def get_today_stats(self):
         today_stats = 0
+        # Change iterator name of the for loop below. It is not a proper way to iterate using class name as an iterator
+        # because iterator in this case is a item of the self.records list which type is int and int has no
+        # date(Record.date) attribute.For example
+        # for spent in self.records:
+        #    ...
         for Record in self.records:
+            # Here need to be created an object of the Record class. And object count will be equal to len(self.records)
             if Record.date == dt.datetime.now().date():
                 today_stats = today_stats + Record.amount
         return today_stats
@@ -30,9 +43,11 @@ class Calculator:
         week_stats = 0
         today = dt.datetime.now().date()
         for record in self.records:
+            # This if statement can be simplified using style mentioned bellow comment.
+            # (7 > (today - record.date).days >= 0)
             if (
-                (today - record.date).days < 7 and
-                (today - record.date).days >= 0
+                    (today - record.date).days < 7 and
+                    (today - record.date).days >= 0
             ):
                 week_stats += record.amount
         return week_stats
@@ -45,13 +60,14 @@ class CaloriesCalculator(Calculator):
             return f'Сегодня можно съесть что-нибудь' \
                    f' ещё, но с общей калорийностью не более {x} кКал'
         else:
-            return('Хватит есть!')
+            return ('Хватит есть!') # In this line for return need not use parentheses.
 
 
 class CashCalculator(Calculator):
     USD_RATE = float(60)  # Курс доллар США.
     EURO_RATE = float(70)  # Курс Евро.
 
+    # In Python argument name should be lowercase. For instance, usd_rate
     def get_today_cash_remained(self, currency,
                                 USD_RATE=USD_RATE, EURO_RATE=EURO_RATE):
         currency_type = currency
@@ -63,8 +79,11 @@ class CashCalculator(Calculator):
             cash_remained /= EURO_RATE
             currency_type = 'Euro'
         elif currency_type == 'rub':
+            # Statement seems to have no effect. It means for assigning we use only one equal sign (x = 10)
             cash_remained == 1.00
             currency_type = 'руб'
+        # In each line you have written currency_type == smth.I would suggest to use currency_type.upper().
+        # And before returning cash_remained in this string  f'{currency_type}' will be appeared USD, РУБ, or EURO
         if cash_remained > 0:
             return (
                 f'На сегодня осталось {round(cash_remained, 2)} '
